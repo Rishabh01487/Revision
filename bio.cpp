338566adadf5dfdbc9681b4dc4d786fd6ede9843
Rishabh Gupta <rishabhgupta999175@gmail.com>
2026-03-31 19:18:43 +0000

    Add bio project with C++ program and VS Code build task

---
+++ bio.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+int main() {
+    string Name, Address;
+    int phoneNo;
+    cout<<"enter your name\n";
+    cin>>Name;
+    cout<<"My name is:\n"<<Name;
+    cout<<"\n enter your address:";
+    cin>>Address;
+    cout<<"My address is:\n"<<Address;
+    cout<<"\n enter your phone no:\n";
+    cin>>phoneNo;
+    cout<<"my phoneNo is:\n "<<phoneNo;
+    return 0;
+}
